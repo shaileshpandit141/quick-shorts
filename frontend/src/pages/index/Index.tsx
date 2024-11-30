9cfbd0cfed588413a4823dea952f63f655b09263
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-11-30 15:45:20 +0530

    Update the Index.tsx component and delete lazyImport.tsx and lazyMetrialIcon.tsx components

--- frontend/src/pages/index/Index.tsx
+++ frontend/src/pages/index/Index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Index.css'
+// import { LazyIconImport } from 'lazyUtils/lazyIconImport'
 
 const Index: React.FC = (props) => {
   return (
-    <div className='inner-grid-1-1 index'>Index</div>
+    <div className='inner-grid-1-1 index'>
+      Index
+    </div>
   )
 }
 
