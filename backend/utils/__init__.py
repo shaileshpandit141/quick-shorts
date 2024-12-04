9512942c4caa734504eb2783291468359f4aef5e
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-04 18:12:07 +0530

    Change the mode of directory backend/utils  to module

---
+++ backend/utils/__init__.py
@@ -0,0 +1,9 @@
+from .response import Response
+from .send_email import SendEmail
+from .token_generator import TokenGenerator
+
+__all__ = [
+    'Response',
+    'SendEmail',
+    'TokenGenerator'
+]
