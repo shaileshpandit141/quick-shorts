30277906edde28078ef85bfbe1c2a94ff96383c4
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-04 18:11:16 +0530

    Change the mode of file backend/permissions to module

---
+++ backend/permissions/__init__.py
@@ -0,0 +1,9 @@
+from .allow_any import AllowAny
+from .is_authenticated  import IsAuthenticated
+from .is_email_verified import IsEmailVerified
+
+__all__ = [
+    'AllowAny',
+    'IsAuthenticated',
+    'IsEmailVerified'
+]
