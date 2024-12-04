9cea3b2e4baa32758d9c2a44ffcebb884739c651
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-04 18:05:33 +0530

    Change the response message if user email is not varified

--- backend/permissions/is_email_verified.py
+++ backend/permissions/is_email_verified.py
@@ -1,11 +1,19 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
+
 
 class IsEmailVerified(BasePermission):
     def has_permission(self, request, view):
         if request.user.is_superuser:
             return True
-        return (
-            request.user.is_authenticated
-            and hasattr(request.user, 'is_email_verified')
-            and request.user.is_email_verified
-        )
+
+        if not hasattr(request.user, 'is_email_verified') or not request.user.is_email_verified:
+            raise PermissionDenied({
+                "status": "error",
+                "message": "Please verify your email to continue.",
+                "errors": {
+                    'detail': ["You must verify your email address to access this resource."]
+                }
+            })
+
+        return True
