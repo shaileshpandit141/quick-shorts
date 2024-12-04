e81039cf285bc2282d9cabe2a17cb3e4ace6c6e1
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-04 17:59:27 +0530

    Add a backend/permissions/is_authenticated.py to change the response message is user is not authorize

---
+++ backend/permissions/is_authenticated.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import IsAuthenticated as DRFIsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
+
+class IsAuthenticated(DRFIsAuthenticated):
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            # Raising a custom permission denied exception with a custom message
+            raise PermissionDenied({
+                "status": "error",
+                "message": "Please sign in to continue.",
+                "errors": {
+                    'detail': ["You must be authenticated to access this resource."]
+                }
+            })
+        return True
