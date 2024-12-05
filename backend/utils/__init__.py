634ec6984843a1b00a56c4daa12971d069cf93fc
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-05 23:54:49 +0530

    Update the utils/__init__.py with add_query_params

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,8 +1,10 @@
+from .add_query_params import add_query_params
 from .response import Response
 from .send_email import SendEmail
 from .token_generator import TokenGenerator
 
 __all__ = [
+    'add_query_params',
     'Response',
     'SendEmail',
     'TokenGenerator'
