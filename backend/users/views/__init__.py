323cc8fefe415678a3cf12aad69072757be33e4a
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-06 22:48:18 +0530

    Add the ChangePasswordAPIView in users/views/__init__.py

--- backend/users/views/__init__.py
+++ backend/users/views/__init__.py
@@ -1,5 +1,5 @@
+from .change_password_apiview import ChangePasswordAPIView
 from .generate_email_verification_link_apiview import GenerateEmailVerificationLinkAPIView
-from .password_change_apiview import PasswordChangeAPIView
 from .signin_token_apiview import SigninTokenAPIView
 from .signin_token_refresh_apiview import SigninTokenRefreshAPIView
 from .signup_apiview import SignupAPIView
@@ -7,8 +7,8 @@ from .user_info_apiview import UserInfoAPIView
 from .verify_email_apiview import VerifyEmailAPIView
 
 __all__ = [
+    'ChangePasswordAPIView',
     'GenerateEmailVerificationLinkAPIView',
-    'PasswordChangeAPIView',
     'SigninTokenAPIView',
     'SigninTokenRefreshAPIView',
     'SignupAPIView',
