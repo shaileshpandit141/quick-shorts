52d9a8b63300ed8bcec9e1d885fe4375d41f5f8e
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-08 15:22:14 +0530

    core(backend/utils): Add a FieldValidator in __init__.py

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,10 +1,12 @@
 from .add_query_params import add_query_params
+from .field_validator import FieldValidator
 from .response import Response
 from .send_email import SendEmail
 from .token_generator import TokenGenerator
 
 __all__ = [
     'add_query_params',
+    'FieldValidator',
     'Response',
     'SendEmail',
     'TokenGenerator'
