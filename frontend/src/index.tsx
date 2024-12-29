04ec2115119f29a107865e9b17e3d49733b15c12
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-12-29 16:20:00 +0530

    core(PWA): rigister the service worker

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store/store'
 import App from './App'
 import './styles/index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,3 +16,8 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
