a3d78f2259723b2c6d5b0a159a629d0045054f05
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-04 17:53:37 +0530

    fix(signin): fixed the bug appair during signout and signin again

--- frontend/src/pages/SigninPage/SigninPage.tsx
+++ frontend/src/pages/SigninPage/SigninPage.tsx
@@ -1,19 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './SigninPage.css'
 import { Navigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { resetSigninState } from 'features/auth'
 import { isAuthenticated } from 'utils/isAuthenticted'
 import SigninHeader from './SigninHeader/SigninHeader'
 import SigninForm from './SigninForm/SigninForm'
 
 const SigninPage: React.FC = (props) => {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(resetSigninState())
-  });
-
   if (isAuthenticated()) {
     return (
       <Navigate to={'/home'} />
