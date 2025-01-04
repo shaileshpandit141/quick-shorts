952e25082dd5c5c3e461210de43bd8dfbd617146
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-04 12:51:05 +0530

    feat(signin): reset the pravious signin state if the use comes again

--- frontend/src/pages/SigninPage/SigninPage.tsx
+++ frontend/src/pages/SigninPage/SigninPage.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './SigninPage.css'
 import { Navigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { resetSigninState } from 'features/auth'
 import { isAuthenticated } from 'utils/isAuthenticted'
 import SigninHeader from './SigninHeader/SigninHeader'
 import SigninForm from './SigninForm/SigninForm'
 
 const SigninPage: React.FC = (props) => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(resetSigninState())
+  });
+
   if (isAuthenticated()) {
     return (
       <Navigate to={'/home'} />
     )
   }
+
   return (
     <div className='signin-page'>
       <SigninHeader />
