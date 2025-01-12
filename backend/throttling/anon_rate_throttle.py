5af8565ad14c0dc5aa1d9156c9b71a25488c107f
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-12 14:46:09 +0530

    rename the backend/throttles to backend/throttling

---
+++ backend/throttling/anon_rate_throttle.py
@@ -0,0 +1,29 @@
+from rest_framework.throttling import (
+    AnonRateThrottle as DRFAnonRateThrottle
+)
+from quick_utils.as_api_response_format import as_api_response_format
+
+class AnonRateThrottle(DRFAnonRateThrottle):
+    def throttle_error_message(self):
+            rate_limit = self.get_rate()
+            wait_time = self.wait()
+            if wait_time is not None:
+                retry_after = str(int(wait_time))
+            else:
+                retry_after = "0"
+
+            return as_api_response_format({
+                "status": "failed",
+                "message": "Sorry, you have made too many requests.",
+                "data": None,
+                "errors": [{
+                    "field": "request",
+                    "code": "rate_limit_exceeded",
+                    "message": "API rate limit has been reached. Please slow down your request rate.",
+                    "details": {
+                        "limit": rate_limit,
+                        "reset_time": int(self.timer()),
+                        "retry_after": retry_after
+                    }
+                }]
+            })
