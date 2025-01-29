33da0592300ccb6805f3bfd3cc030f8904a5a6d0
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-29 14:26:49 +0530

    refactor: remove the header in AuthLauout component and add it as unvisible in RootLayout

--- frontend/src/layouts/AuthLayout/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import './AuthLayout.css'
 import { Outlet } from 'react-router-dom'
-import { ToggleThemeButton } from 'components'
 
 const AuthLayout: React.FC = (props) => {
   return (
     <div className='inner-grid-1-1 auth-alyout'>
-      <div className='grid-12 theme-button-wrapper'>
-        <div className='inner-grid-2-2 theme-button-container'>
-          <ToggleThemeButton />
-        </div>
-      </div>
       <div className='form-wrapper'>
         <Outlet />
       </div>
--- frontend/src/layouts/RootLayout/RootLayout.tsx
+++ frontend/src/layouts/RootLayout/RootLayout.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './RootLayout.css';
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'components';
+import { ToastContainer, ToggleThemeButton } from 'components';
 
 const RootLayout: React.FC = () => {
   return (
     <>
+      <div className='global-theme-button-container'>
+        <ToggleThemeButton />
+      </div>
       <ToastContainer />
       <Outlet />
     </>
