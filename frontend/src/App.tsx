6a25d5402fafb54a1abbf63fbf75949456292334
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-02-25 14:02:11 +0530

    refactor: remove the AddSEO component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,8 @@
 import Routes from "routes/Routes";
-import { AddSEO, appMetaConfig } from "SEO";
 
 // App entry point.
 const App = (): JSX.Element => {
-  return <>
-    <AddSEO
-      title={appMetaConfig.appName}
-      description="Welcome to my website, where you can find the best content."
-      keywords="home, react, SEO, optimization"
-    />
-    <Routes />
-  </>;
+  return <Routes />;
 };
 
 export default App;
