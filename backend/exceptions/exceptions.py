627cc731279e4c39db2e5ab09efd4913086a8cd4
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-13 18:07:16 +0530

    feat: handle jwt token related errors

--- backend/exceptions/exceptions.py
+++ backend/exceptions/exceptions.py
@@ -10,11 +10,17 @@ from rest_framework.exceptions import (
     ValidationError,
     APIException,
 )
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from django.core.cache import cache
+from datetime import datetime
+import pytz
+from throttling import AnonRateThrottle
 
 
 def exception_handler(exc, context) -> Response | views.Response | None:
     """A custom exception handler that returns the exception details in a custom format."""
     response = views.exception_handler(exc, context)
+    request = context.get("request", None)
 
     error_handlers = {
         ValidationError: lambda error: create_error_response(
@@ -47,12 +53,23 @@ def exception_handler(exc, context) -> Response | views.Response | None:
             errors=error,
             status=status.HTTP_429_TOO_MANY_REQUESTS,
         ),
+        InvalidToken: lambda error: create_error_response(
+            message="Invalid token: token is invalid or expired",
+            errors=error,
+            status=status.HTTP_401_UNAUTHORIZED,
+        ),
+        TokenError: lambda error: create_error_response(
+            message="Token error: please provide a valid token",
+            errors=error,
+            status=status.HTTP_401_UNAUTHORIZED,
+        ),
         APIException: lambda error: create_error_response(
             message="Something went wrong. Please try again later",
             errors=error,
         ),
     }
 
+    # Handle all exceptions
     for exception_class, handler in error_handlers.items():
         if isinstance(exc, exception_class):
             response = handler(exc)
@@ -62,6 +79,62 @@ def exception_handler(exc, context) -> Response | views.Response | None:
                 )
             elif isinstance(exc, Exception):
                 response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+            # Apply throttling if any exception is raised
+            if request is not None:
+                # Get the view (if available)
+                view = context.get("view", None)
+
+                is_authenticated = request.user.is_authenticated
+                if is_authenticated:
+                    # Use view-defined throttle classes or fallback to AnonRateThrottle
+                    throttle_classes = getattr(view, "throttle_classes", None) or [
+                        AnonRateThrottle
+                    ]
+                else:
+                    throttle_classes = [AnonRateThrottle]
+                    setattr(view, "throttle_classes", throttle_classes)
+
+                # Iterate over the list of throttles class
+                for throttle_class in throttle_classes:
+                    throttle = throttle_class()  # Instantiate the throttle class
+                    cache_key = throttle.get_cache_key(request, view)
+
+                    if cache_key:
+                        history = cache.get(cache_key, [])
+                        now = datetime.now(pytz.UTC).timestamp()
+
+                        # Remove expired requests from history
+                        history = [
+                            timestamp
+                            for timestamp in history
+                            if now - timestamp < throttle.duration
+                        ]
+
+                        # Check if throttle limit is exceeded or not
+                        if isinstance(throttle.num_requests, int):
+                            if len(history) >= throttle.num_requests:
+                                retry_after = throttle.duration
+                                if history:
+                                    retry_after = int(
+                                        throttle.duration - (now - history[0])
+                                    )
+
+                                return create_error_response(
+                                    message="Opps! request limit exceeded",
+                                    errors={
+                                        "detail": "Opps! request limit exceeded. Please try again later.",
+                                        "retry_after": f"{retry_after} seconds",
+                                    },
+                                    status=status.HTTP_429_TOO_MANY_REQUESTS,
+                                )
+
+                        # Otherwise, add the current request to history and update cache
+                        history.append(now)
+                        cache.set(cache_key, history, throttle.duration)
+
+            # Return updaed response instance
             return response
 
+    # Return un updaed response instance
     return response
