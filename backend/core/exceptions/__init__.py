2ad2ba4907748b77782cd0bfbfe7e5472f15e234
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-13 18:16:05 +0530

    refactor: move exceptions directory to core directory

---
+++ backend/core/exceptions/__init__.py
@@ -0,0 +1,3 @@
+from .exceptions import exception_handler
+
+__all__ = ["exception_handler"]
---
+++ backend/core/exceptions/create_error_response/__init__.py
@@ -0,0 +1,3 @@
+from .create_error_response import create_error_response
+
+__all__ = ["create_error_response"]
