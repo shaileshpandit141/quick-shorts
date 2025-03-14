4a8a69b6e7f3800c73d89f571f4405f78400c5f0
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-14 23:12:20 +0530

    feat: add display error and success detail components

---
+++ frontend/src/components/DisplayErrorDetails/DisplayErrorDetails.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./DisplayErrorDetails.css";
+
+interface Props {
+  details: string | string[] | undefined
+}
+
+const DisplayErrorDetails: React.FC<Props> = ({ details }) => {
+
+  if (details === undefined) {
+    return null
+  }
+
+  if (typeof details === "string") {
+    return (
+      <div className="error-details-container">
+        <p className="detail">{details}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="error-details-container">
+      {details.map((detail, index) => (
+        <p className="detail" key={index}>
+          {detail}
+        </p>
+      ))}
+    </div>
+  )
+
+};
+
+export default DisplayErrorDetails;
---
+++ frontend/src/components/DisplaySuccessDetails/DisplaySuccessDetails.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./DisplaySuccessDetails.css";
+
+interface Props {
+  details: string | string[] | undefined
+}
+
+const DisplaySuccessDetails: React.FC<Props> = ({ details }) => {
+
+  if (details === undefined) {
+    return null
+  }
+
+  if (typeof details === "string") {
+    return (
+      <div className="success-details-container">
+        <p className="detail">{details}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="success-details-container">
+      {details.map((detail, index) => (
+        <p className="detail" key={index}>
+          {detail}
+        </p>
+      ))}
+    </div>
+  )
+
+};
+
+export default DisplaySuccessDetails;
