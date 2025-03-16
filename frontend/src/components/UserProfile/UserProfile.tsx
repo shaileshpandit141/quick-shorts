97ba79d6c7a8c41beb596501b06afd6102ca026f
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-16 11:27:00 +0530

    fix: resolve the image not found bug

--- frontend/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/components/UserProfile/UserProfile.tsx
@@ -4,7 +4,6 @@ import { NavLink, SignoutButton } from "components";
 import { useMenu } from "hooks";
 import { user, useUserSelector } from "features/user";
 import { isUserAuthenticated } from "utils/isUserAuthenticated";
-import { buildMediaURL } from "utils/buildMediaURL";
 import { getEnv } from "utils/getEnv";
 
 const UserProfile: React.FC = (): JSX.Element | null => {
@@ -43,14 +42,14 @@ const UserProfile: React.FC = (): JSX.Element | null => {
     <div className="user-profile">
       <button className="button-as-icon profile-action-button" ref={buttonRef}>
         {"avatar" in data && (
-          <img src={buildMediaURL(data.avatar)} alt="avatar-image" />
+          <img src={data.avatar} alt="avatar-image" />
         )}
       </button>
       <div className="user-profile-card-container" ref={userProfileRef}>
         <div className="user-profile-header-card">
           <section className="user-profile-image">
             {"avatar" in data && (
-              <img src={buildMediaURL(data.avatar)} alt="avatar-image" />
+              <img src={data.avatar} alt="avatar-image" />
             )}
           </section>
           <section className="user-profile-info">
