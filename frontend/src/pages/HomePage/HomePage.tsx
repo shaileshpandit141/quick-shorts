5f23ff5ed3aba8274ad521fe2f8176e4cce51c4c
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-17 17:46:32 +0530

    feat: update meta content in home page

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./HomePage.css";
+import { AddSEO } from "SEO";
 
 const HomePage: React.FC = (props) => {
   return (
     <div className="grid-start-2-end-2">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quod soluta
-      officia molestias recusandae asperiores est perferendis dolorum! Id
-      reiciendis fugiat laboriosam doloremque quae ratione iusto enim, cumque
-      atque magni.
+      <AddSEO
+        title="Home"
+        description="Welcome to my website, where you can find the best content."
+        keywords="home, react, SEO, optimization"
+      />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quod soluta
+        officia molestias recusandae asperiores est perferendis dolorum! Id
+        reiciendis fugiat laboriosam doloremque quae ratione iusto enim, cumque
+        atque magni.
+      </p>
     </div>
   );
 };
