c60c8455a3d9a9f42aafad821042dc2c7656d30d
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-19 15:01:29 +0530

    style: update error boundary UI styles

--- frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -9,15 +9,17 @@ import Button from "components/Button/Button";
 const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => (
   <div className="inner-grid-2-2 error-boundary" role="alert">
     <div className="error-boundary-component">
-      <Button type="button" icon="info" title={error?.message}>
-        Something went wrong
-      </Button>
       <Button
-        type="icon"
+        type="button"
+        icon="info"
+        title={error?.message}
+      >Something went wrong!</Button>
+      <Button
+        type="button"
         icon="reTry"
         onClick={resetErrorBoundary}
         title="re try"
-      />
+      >re-try</Button>
     </div>
   </div>
 );
