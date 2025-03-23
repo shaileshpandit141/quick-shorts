3bb13352c23a6416e566b9a76022004f5e17d0f2
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-23 15:43:30 +0530

    feat: update footer with copyright elements

--- frontend/src/components/Layout/MainLayout/Footer/Footer.tsx
+++ frontend/src/components/Layout/MainLayout/Footer/Footer.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = (props) => {
+  const copyRightYear = new Date().getFullYear();
+
   return (
-    <footer className="inner-grid-1-1 grid-12 footer">
-      <div className="inner-grid-2-2 wrapper">Footer</div>
+    <footer className="grid-12 footer">
+      <div className="grid-start-2-end-2 footer-wrapper">
+        <div className="content">
+          {/* Footer TSX content goes here */}
+          Footer
+        </div>
+        <section className="copy-right-container">
+          <p className="copy-right-text">
+            &copy; {copyRightYear} YourCompany. All rights reserved. &#124;
+            <Link to="/privacy-policy" className="links"> Privacy Policy </Link>&#124;
+            <Link to="/terms" className="links"> Terms of Service</Link>
+          </p>
+        </section>
+      </div>
     </footer>
   );
 };
