823cc0aa99bc44ce3c8773abd84f223bbe96c606
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-04-04 00:23:20 +0530

    chore: remove rest_framework.authtoken configuration

--- backend/apps_config/settings/base.py
+++ backend/apps_config/settings/base.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 # ---------------------------------
 INSTALLED_APPS += [
     "rest_framework",
-    "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
