9efab23b1865eee2f189666c4b6fda845d4dbf06
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-15 20:12:20 +0530

    feat: define like model.

---
+++ backend/apps/short_video/models/like.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from .short_video import ShortVideo
+
+User = get_user_model()
+
+
+class Like(models.Model):
+    """User likes a video"""
+
+    class Meta:
+        db_table = "like"
+        verbose_name = "like"
+        verbose_name_plural = "likes"
+        ordering = ["-id"]
+        unique_together = ("video", "user")
+
+    objects = models.Manager()
+
+    # Model fields for Like
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=False,
+        related_name="likes",
+        related_query_name=None,
+        limit_choices_to={},
+        parent_link=False,
+        blank=False,
+        null=False,
+        db_index=True,
+        db_constraint=True,
+        error_messages={
+            "invalid": "Invalid value",
+            "invalid_choice": "Select a valid user.",
+            "does_not_exist": "Object does not exist",
+        },
+    )
+    video = models.ForeignKey(
+        ShortVideo,
+        on_delete=models.CASCADE,
+        primary_key=False,
+        related_name="likes",
+        related_query_name=None,
+        limit_choices_to={},
+        parent_link=False,
+        blank=False,
+        null=False,
+        db_index=True,
+        db_constraint=True,
+        error_messages={
+            "invalid": "Invalid value",
+            "invalid_choice": "Select a valid choice.",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank",
+            "does_not_exist": "Object does not exist",
+        },
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
