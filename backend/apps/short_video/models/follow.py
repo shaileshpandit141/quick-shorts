b40b4c29aafe8105b7c8a1378bd61b2d48c60374
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-15 20:13:09 +0530

    feat: define follow model.

---
+++ backend/apps/short_video/models/follow.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+    """Follow system"""
+
+    class Meta:
+        db_table = "follow"
+        verbose_name = "follow"
+        verbose_name_plural = "follows"
+        ordering = ["-id"]
+        unique_together = ("follower", "following")
+
+    objects = models.Manager()
+
+    # Model fields for Follow
+    follower = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following",
+        blank=False,
+        null=False,
+        db_index=True,
+        error_messages={
+            "invalid": "Invalid value",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank",
+            "does_not_exist": "Object does not exist",
+        },
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="followers",
+        blank=False,
+        null=False,
+        db_index=True,
+        error_messages={
+            "invalid": "Invalid value",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank",
+            "does_not_exist": "Object does not exist",
+        },
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
