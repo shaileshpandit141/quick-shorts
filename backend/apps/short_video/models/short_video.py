ae47016bea16971ee0bf5d179dd8c9059b64ea6f
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-15 20:11:35 +0530

    feat: define short video model.

---
+++ backend/apps/short_video/models/short_video.py
@@ -0,0 +1,130 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from .tag import Tag
+
+User = get_user_model()
+
+
+class ShortVideo(models.Model):
+    """ShortVideo model class"""
+
+    class Meta:
+        db_table = "short_video"
+        verbose_name = "short video"
+        verbose_name_plural = "short videos"
+        ordering = ["-id"]
+
+    objects = models.Manager()
+
+    PRIVACY_CHOICES = [
+        ("public", "Public"),
+        ("private", "Private"),
+    ]
+
+    # Model fields for ShortVideo
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=False,
+        related_name="shorts",
+        related_query_name=None,
+        limit_choices_to={},
+        parent_link=False,
+        blank=False,
+        null=False,
+        db_index=True,
+        db_constraint=True,
+        error_messages={
+            "invalid": "Invalid value",
+            "invalid_choice": "Select a valid choice",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank",
+            "does_not_exist": "Object does not exist"
+        }
+    )
+    title = models.CharField(
+        max_length=120,
+        unique=True,
+        blank=False,
+        null=False,
+        db_index=True,
+        error_messages={
+            "invalid": "Invalid value",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank",
+            "max_length": "Ensure this value has at most 120 characters",
+        }
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        db_index=False,
+        default="",
+        error_messages={
+            "invalid": "Enter a valid value",
+        }
+    )
+    video = models.FileField(
+        upload_to="/shorts/videos/",
+        max_length=100,
+        blank=False,
+        null=False,
+        storage=None,
+        db_index=False,
+        default=None,
+        error_messages={
+            "invalid": "No file was submitted",
+            "missing": "No file was submitted",
+            "empty": "The submitted file is empty",
+            "max_length": "Ensure this filename has at most 100 characters",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank"
+        }
+    )
+    thumbnail = models.ImageField(
+        upload_to="shorts/thumbnails/",
+        max_length=100,
+        blank=True,
+        null=True,
+        storage=None,
+        db_index=False,
+        default=None,
+        error_messages={
+            "invalid": "Invalid image file",
+            "invalid_image": "Upload a valid image.",
+            "missing": "No file was submitted",
+            "empty": "The submitted file is empty",
+            "max_length": "Ensure this filename has at most 100 characters",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank"
+        }
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="short_videos",
+        related_query_name=None,
+        db_constraint=True,
+        blank=False,
+        error_messages={
+            "invalid": "Invalid value",
+            "invalid_choice": "Select a valid tags.",
+            "invalid_pk_value": "Invalid primary key value",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank"
+        }
+    )
+    privacy = models.CharField(
+        max_length=10,
+        unique=True,
+        blank=True,
+        null=True,
+        db_index=True,
+        choices=PRIVACY_CHOICES,
+        default="public",
+        error_messages={
+            "invalid": "Please choose a valid privacy",
+            "max_length": "Ensure this value has at most 10 characters",
+        },
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
