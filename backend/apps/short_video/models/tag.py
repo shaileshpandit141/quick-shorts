df4fc281ee6d29d3848eb51fa98dc6741f9cb44d
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-15 20:11:09 +0530

    feat: define tag model.

---
+++ backend/apps/short_video/models/tag.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    """Hashtags"""
+
+    class Meta:
+        db_table = "tag"
+        verbose_name = "tag"
+        verbose_name_plural = "tags"
+        ordering = ["-id"]
+
+    objects = models.Manager()
+
+    # Model fields for Tag
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        blank=False,
+        null=False,
+        db_index=True,
+        default="",
+        error_messages={
+            "invalid": "Invalid value",
+            "null": "This field cannot be null",
+            "blank": "This field cannot be blank",
+            "max_length": "Ensure this value has at most 50 characters",
+        }
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
