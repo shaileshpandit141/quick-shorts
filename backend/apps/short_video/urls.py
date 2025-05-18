3ffcde8d9d49bdcd6ec7c030f92b58723f00bb54
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-18 18:55:06 +0530

    feat: define crud action view for like model.

--- backend/apps/short_video/urls.py
+++ backend/apps/short_video/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
+from .views.like_view import LikeViewModelViewSet
 from .views.short_video_view import ShortVideoModelViewSet
 from .views.short_video_view_view import ShortVideoViewModelViewSet
 from .views.tag_view import TagModelViewSet
@@ -18,6 +19,9 @@ router.register(r"tags", TagModelViewSet, basename="tag")
 
 # Register the ShortVideoViewModelViewSet with the router
 router.register(r"views", ShortVideoViewModelViewSet, basename="view")
+
+# Register the LikeViewModelViewSet with the router
+router.register(r"likes", LikeViewModelViewSet, basename="like")
 
 # Include the router's URLs in the urlpatterns
 urlpatterns += router.urls
--- backend/apps/short_video/views/like_view.py
+++ backend/apps/short_video/views/like_view.py
@@ -2,22 +2,22 @@ from rest_core.viewsets.mixins import ChoiceFieldViewSetMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.viewsets import ModelViewSet
-from short_video.models.view import View
-from short_video.serializers.view_serializer import ViewSerializer
+from short_video.models.like import Like
+from short_video.serializers.like_serializer import LikeSerializer
 
 
-class ShortVideoViewModelViewSet(
+class LikeViewModelViewSet(
     ChoiceFieldViewSetMixin,
     ModelViewSet,
 ):
-    """Short video view CRUD API view"""
+    """Like view CRUD API view"""
 
     permission_classes = [IsAuthenticated]
     throttle_classes = [UserRateThrottle]
-    queryset = View.objects.all()
-    serializer_class = ViewSerializer
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
     lookup_field = "id"
 
     def perform_create(self, serializer) -> None:
-        """Create a new short video view with the user."""
+        """Create a new like video with the owner."""
         serializer.save(user=self.request.user)
