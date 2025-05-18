37adfd36e8a885ff92317d092793d02b9d55be35
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-18 16:44:24 +0530

    refactor: format tag view.

--- backend/apps/short_video/views/tag_view.py
+++ backend/apps/short_video/views/tag_view.py
@@ -1,9 +1,9 @@
+from rest_core.viewsets.mixins import ChoiceFieldViewSetMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.throttling import UserRateThrottle
+from rest_framework.viewsets import ModelViewSet
 from short_video.models.tag import Tag
 from short_video.serializers.tag_serializer import TagSerializer
-from rest_core.viewsets.mixins import ChoiceFieldViewSetMixin
-from rest_framework.viewsets import ModelViewSet
 
 
 class TagModelViewSet(
