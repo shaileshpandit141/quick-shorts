bfd63e5212ff009aabe1eadc33d3de217e3c869b
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-25 16:30:46 +0530

    feat: make user read only field.

--- backend/apps/short_video/serializers/like_serializer.py
+++ backend/apps/short_video/serializers/like_serializer.py
@@ -9,4 +9,4 @@ class LikeSerializer(RecordsCreationMixin, ModelSerializer):
     class Meta:
         model = Like
         fields = ["id", "user", "video", "liked_at"]
-        read_only_fields = ["id", "liked_at"]
+        read_only_fields = ["id", "user", "liked_at"]
