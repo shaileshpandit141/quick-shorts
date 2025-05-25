f03c882ed6d1886ea93f1d784efa586f5162f6bf
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-25 12:13:51 +0530

    feat: add a permission for user can update and delete records.

---
+++ backend/apps/short_video/permissions.py
@@ -0,0 +1,12 @@
+from typing import Any, Literal
+from rest_framework.permissions import BasePermission
+from short_video.models.short_video import ShortVideo
+
+
+class CanUpdateAndDelete(BasePermission):
+    def has_object_permission(
+        self, request, view, obj: ShortVideo
+    ) -> Any | Literal[True]:
+        if request.method in ("PUT", "PATCH", "DELETE"):
+            return obj.owner == request.user
+        return True
