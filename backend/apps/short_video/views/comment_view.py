b904307a29632b1560562b6830790c919ef6828a
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-26 11:06:44 +0530

    feat: add filters in short video and comment view.

--- backend/apps/short_video/views/comment_view.py
+++ backend/apps/short_video/views/comment_view.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from short_video.models.comment import Comment
 from short_video.permissions import CanUpdateAndDelete
 from short_video.serializers.comment_serializer import CommentSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CommentModelViewSet(ModelViewSet):
     """Comment view CRUD API view"""
@@ -14,6 +14,8 @@ class CommentModelViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     lookup_field = "id"
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["video"]
 
     def perform_create(self, serializer) -> None:
         """Create a new comment view with the user."""
--- backend/apps/short_video/views/short_video_view.py
+++ backend/apps/short_video/views/short_video_view.py
@@ -5,6 +5,8 @@ from rest_framework.viewsets import ModelViewSet
 from short_video.models.short_video import ShortVideo
 from short_video.permissions import CanUpdateAndDelete
 from short_video.serializers.short_video_serializer import ShortVideoSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class ShortVideoModelViewSet(
@@ -19,6 +21,14 @@ class ShortVideoModelViewSet(
     serializer_class = ShortVideoSerializer
     lookup_field = "id"
     choice_fields = ["privacy"]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ["owner", "tags"]
+    search_fields = ["title", "description"]
+    ordering_fields = ["id", "updated_at"]
 
     def perform_create(self, serializer) -> None:
         """Create a new short video with the owner."""
