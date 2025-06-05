710dd2cc720fd59f87a1463c9fbb94c70c48918c
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-06-05 13:25:36 +0530

    feat: add number formatter hook.

---
+++ frontend/src/hooks/useNumberFormatter.ts
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+
+type FormatOptions = {
+  decimals?: number;
+};
+
+const useNumberFormatter = () => {
+  const formatNumber = useCallback(
+    (num: number, options: FormatOptions = {}) => {
+      const { decimals = 1 } = options;
+
+      if (num < 1000) return num.toString();
+
+      const units = ["K", "M", "B", "T"];
+      let unitIndex = -1;
+      let reducedNum = num;
+
+      while (reducedNum >= 1000 && unitIndex < units.length - 1) {
+        reducedNum /= 1000;
+        unitIndex++;
+      }
+
+      return `${parseFloat(reducedNum.toFixed(decimals))}${units[unitIndex]}`;
+    },
+    []
+  );
+
+  return formatNumber;
+};
+
+export default useNumberFormatter;
