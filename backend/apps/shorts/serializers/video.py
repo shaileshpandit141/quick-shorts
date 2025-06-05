37e7e94f152c8135bb6fcdd093199bd076e587e7
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-06-05 13:17:47 +0530

    feat: add total views, likes comments field.

--- backend/apps/shorts/serializers/video.py
+++ backend/apps/shorts/serializers/video.py
@@ -1,7 +1,10 @@
 from rest_core.serializers.mixins import FileFieldUrlMixin, RecordsCreationMixin
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from shorts.models.video import Video
 from user_auth.serializers.user_serializers import UserPublicSerializer
+from shorts.models.comment import Comment
+from shorts.models.view import View
+from shorts.models.like import Like
 
 from .tag import TagSerializer
 
@@ -12,6 +15,9 @@ class VideoSerializer(RecordsCreationMixin, FileFieldUrlMixin, ModelSerializer):
     # Call nested serializers
     owner = UserPublicSerializer(read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+    total_views = SerializerMethodField()
+    total_likes = SerializerMethodField()
+    total_comments = SerializerMethodField()
 
     class Meta:
         model = Video
@@ -22,7 +28,23 @@ class VideoSerializer(RecordsCreationMixin, FileFieldUrlMixin, ModelSerializer):
             "thumbnail",
             "caption",
             "tags",
+            "total_views",
+            "total_likes",
+            "total_comments",
             "privacy",
             "updated_at",
         ]
-        read_only_fields = ["id", "owner", "updated_at"]
+        read_only_fields = [
+            "id",
+            "owner",
+            "updated_at",
+        ]
+
+    def get_total_views(self, obj) -> int:
+        return View.objects.filter(video=obj.id).count()
+
+    def get_total_likes(self, obj) -> int:
+        return Like.objects.filter(video=obj.id).count()
+
+    def get_total_comments(self, obj) -> int:
+        return Comment.objects.filter(video=obj.id).count()
